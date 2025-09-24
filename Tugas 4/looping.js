f417d1926aa62e7432dc83924772a6784608a548
norlaklep <fajar.kadir30@gmail.com>
2025-09-24 15:48:13 +0700

    Tugas (4) ==> Looping Pertama dan Looping Kedua

---
+++ Tugas 4/looping.js
@@ -0,0 +1,9 @@
+console.log('LOOPING PERTAMA'); 
+for (let i = 2; i <= 20; i += 2) { 
+  console.log(`${i} - I love coding`);
+}
+
+console.log('LOOPING KEDUA');
+for (let i = 20; i >= 2; i -= 2) { 
+  console.log(`${i} - I will become a mobile developer`); 
+}
