513e56d051ac80f6574dbad77854b454de7ede7f
norlaklep <fajar.kadir30@gmail.com>
2025-09-24 15:03:30 +0700

    Tugas (2) ==> Output == MAQDIS Academy is awesome

---
+++ Tugas 2/type.js
@@ -0,0 +1,8 @@
+let variable1 = "MAQDIS";
+let variable2 = "Academy";
+let variable3 = "is";
+let variable4 = "awesome";
+
+let kalimat = `${variable1} ${variable2} ${variable3} ${variable4}`;
+
+console.log(kalimat);
---
+++ Tugas 2/type.php
@@ -0,0 +1,10 @@
+<?php
+
+$variable1 = "MAQDIS";
+$variable2 = "Academy";
+$variable3 = "is";
+$variable4 = "awesome";
+
+$kalimat = $variable1 . " " . $variable2 . " " . $variable3 . " " . $variable4;
+
+echo $kalimat; 
